590e3c3a5d5bcb7e4b040657c6907cde00ace8e8
seheon <quwe153@naver.com>
2022-04-14 17:48:45 +0900

    study query method

--- data-jpa/src/main/java/study/datajpa/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/datajpa/repository/MemberRepository.java
@@ -1,14 +1,18 @@
 package study.datajpa.datajpa.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
 import study.datajpa.datajpa.dto.MemberDto;
 import study.datajpa.datajpa.entity.Member;
 
+import javax.persistence.LockModeType;
+import javax.persistence.QueryHint;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 
@@ -25,4 +29,36 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @Query("select m from Member m where m.userName in :name")   // 파라미터 list 형식으로 받기(in)
     List<Member> findMemberByNames(@Param("name") Collection<String> name);
 
+    List<Member> findCollectionByUserName(String userName); // 컬렉션
+    Member findListByUserName(String serName); // 단건
+    Optional<Member> findOptionalByUserName(String userName); // 단건 optional
+
+//    @Query(value = "select m from Member m join m.team t",
+//            countQuery = "select count(m) from Member m")  // count 따로 뺴기(상황에 따라 성능 향상)
+    Page<Member> findPageByAge(int age, Pageable pageable);
+
+    // @Modifying 써줘야함
+    @Modifying(clearAutomatically = true) // bulk연산은 한방에 db에 때리기 때문에 영속성 context clear시켜줘야함
+    @Query("update Member m set m.age = m.age + 1 where m.age >= :age")
+    int bulkAgePlus(@Param("age") int age);
+
+
+    @Query("select m from Member m join fetch m.team")
+    List<Member> findFetchMember();
+
+    @EntityGraph(attributePaths = {"team"}) // fetch join한 것과 똑같은 코드
+    @Override
+    List<Member> findAll();
+
+    @EntityGraph(attributePaths = {"team"}) // fetch join한 것과 똑같은 코드
+    @Query("select m from Member m")
+    List<Member> findEntityGraphMember();
+
+    // readOnly조건 걸어줌(성능상 확실히 향상될 때만 쓰자, 무차별 x)
+    @QueryHints(value = @QueryHint(name = "org.hibernate.readOnly", value = "true"))
+    List<Member> findReadOnlyByUserName(String userName);
+
+    // 찾아보기, 가급적 쓰지 말길
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    List<Member> findLockByUserName(String userName);
 }
--- data-jpa/src/test/java/study/datajpa/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/datajpa/repository/MemberRepositoryTest.java
@@ -4,26 +4,36 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.datajpa.dto.MemberDto;
 import study.datajpa.datajpa.entity.Member;
 import study.datajpa.datajpa.entity.Team;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.validation.constraints.AssertFalse;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
+@Rollback(value = false)
 public class MemberRepositoryTest {
     @Autowired
     MemberRepository memberRepository;
     @Autowired
     TeamRepository teamRepository;
+    @PersistenceContext
+    EntityManager em;
+
 
     @Test
     public void MemberRepositoryTest(){
@@ -44,6 +54,97 @@ public class MemberRepositoryTest {
 
         List<Member> memberByNames = memberRepository.findMemberByNames(Arrays.asList("박세헌", "서지오"));
         memberByNames.forEach(o-> System.out.println(o.toString()));
+    }
+
+    @Test
+    public void findUser(){
+        Member m1 = new Member("박세헌", 27);
+        Member m2 = new Member("박세헌", 27);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+        Optional<Member> s = memberRepository.findOptionalByUserName("박세헌").ofNullable(null);
+        System.out.println(s);
+    }
+
+    @Test
+    public void pagingTest(){
+        memberRepository.save(new Member("박세헌1", 27));
+        memberRepository.save(new Member("박세헌2", 27));
+        memberRepository.save(new Member("박세헌3", 27));
+        memberRepository.save(new Member("박세헌4", 27));
+        memberRepository.save(new Member("박세헌5", 27));
+
+        int age = 27;
+        PageRequest pageRequest = PageRequest.of(0, 3, Sort.by(Sort.Direction.DESC, "userName"));
+
+        Page<Member> page = memberRepository.findPageByAge(age, pageRequest);
+
+        Page<MemberDto> memberDto = page.map(o -> new MemberDto(o.getId(), o.getUserName(), o.getTeam().getName()));
+        // memberDto로 변환
+
+        List<Member> content = page.getContent(); //조회된 데이터
+
+        assertThat(content.size()).isEqualTo(3); //조회된 데이터 수
+        assertThat(page.getTotalElements()).isEqualTo(5); //전체 데이터 수
+        assertThat(page.getNumber()).isEqualTo(0); //페이지 번호
+        assertThat(page.getTotalPages()).isEqualTo(2); //전체 페이지 번호
+        assertThat(page.isFirst()).isTrue(); //첫번째 항목인가?
+        assertThat(page.hasNext()).isTrue(); //다음 페이지가 있는가?
+
+    }
+
+    @Test
+    public void bulkTest(){
+        memberRepository.save(new Member("박세헌1", 16));
+        memberRepository.save(new Member("박세헌2", 17));
+        memberRepository.save(new Member("박세헌3", 18));
+        memberRepository.save(new Member("박세헌4", 20));
+        memberRepository.save(new Member("박세헌5", 30));
+
+        int count = memberRepository.bulkAgePlus(20);
+
+        assertThat(count).isEqualTo(2);
+    }
+
+    @Test
+    public void entityGraphTest(){
+        Team team1 = new Team("광교");
+        Team team2 = new Team("용인");
+        teamRepository.save(team1);
+        teamRepository.save(team2);
+
+        memberRepository.save(new Member("박세헌1", 16, team1));
+        memberRepository.save(new Member("박세헌2", 17, team2));
+
+        em.flush();
+        em.clear();
+
+
+        List<Member> result = memberRepository.findAll();
+        for (Member member : result) {
+            System.out.println(member);
+            System.out.println(member.getTeam().getName());
+        }
+    }
+
+    @Test
+    public void hintAndLockTest(){
+        Team team1 = new Team("광교");
+        Team team2 = new Team("용인");
+        teamRepository.save(team1);
+        teamRepository.save(team2);
+
+        memberRepository.save(new Member("박세헌1", 16, team1));
+        memberRepository.save(new Member("박세헌2", 17, team2));
+
+        em.flush();
+        em.clear();
+
+        List<Member> m1 = memberRepository.findReadOnlyByUserName("박세헌1");
+        for (Member member : m1) {
+            member.setUserName("1");
+        }
+
     }
 
 }
